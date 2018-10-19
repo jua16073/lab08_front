6b25d7564b957e6683cc685be6401129cf173af0
Unknown <jua16073@uvg.edu.gt>
2018-10-19 00:22:52 -0600

    types, actions

---
+++ chismes/src/actions/index.js
@@ -0,0 +1,34 @@
+import * as types from '../types';
+
+export const addPost = (
+title, 
+body,
+) => ({
+  type: types.POST_ADDED,
+  payload:{
+    title,
+    body,
+  }
+});
+
+export const editPost = (
+  id,
+  title,
+  body,
+) => ({
+  type: types.POST_EDITED,
+  payload: {
+    id,
+    title,
+    body,
+  }
+})
+
+export const erastePost = (
+id
+) => ({
+  type: types.POST_DELETED,
+  payload:{
+    id,
+  }
+})
---
+++ chismes/src/types/index.js
@@ -0,0 +1,3 @@
+export const POST_ADDED = 'POST_ADDED'
+export const POST_EDITED = 'POST_EDITED'
+export const POST_DELETED = 'POST_DELETED'
